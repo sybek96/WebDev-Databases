5873ce46aa9faedf906393c80d5b3b994c4924b2
sybek96 <sybek96@interia.pl>
2018-01-26 10:25:49 +0000

    view called home returns hello,world. altered views.py and urls.py

--- DjangoProject/myproject/myproject/urls.py
+++ DjangoProject/myproject/myproject/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from boards import views
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
