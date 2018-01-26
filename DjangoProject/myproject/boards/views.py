3aed1668d8d3fc101d3bf19921197038adcd26f2
sybek96 <sybek96@interia.pl>
2018-01-26 13:01:28 +0000

    Added a table as admin through the admin account

--- DjangoProject/myproject/boards/views.py
+++ DjangoProject/myproject/boards/views.py
@@ -4,13 +4,6 @@ from .models import Board
 
 def home(request):
     boards= Board.objects.all()
-    boards_names = list()
-
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '<br>'.join(boards_names)
-
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards': boards})
 
 # Create your views here.
